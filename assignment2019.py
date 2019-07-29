d5280a21c9f088c1dab42ffd1e57c4297f9ce3f8
franckoline@hotmail.com <franckoline@hotmail.com>
2019-07-29 01:53:21 +0100

    Updated

--- assignment2019.py
+++ assignment2019.py
@@ -1,33 +1,3 @@
-"""
-
-Specific Tasks
-    1. Create a single polygon from the Union of all the polygons.
-    2. Compute the centroid of the single polygon.
-    3. Extract the points that lie within the single polygon.
-    4. Compute a convex hull and centroid for the extracted points
-    5. Compute the distance between the centroid of the single polygon and the
-    centroid of the points that lie within the single polygon.
-    6. Create a representation of the line joining the two centroids
-    7. Geocode both centroids and add their names to the appropriate point as an
-    attribute
-    8. Create shapefiles to store the results of the above. Bear in mind that a shapefile
-    contains a single geometry type and is a set of thematically related features.
-    Therefore you will need to create shapefiles as follows:
-        • Combined polygon from Union
-        • Points that lie within Combined Polygon
-        • Convex hull of the points from above
-        • Both centroids. Each should have an attribute to hold its name returned
-        from the geocoding process.
-        • Linestring representing the distance between the centroids
-
-Process
-Data Downloads
-dit data preview using LEGAL TOWN GEOM
-Format in Geojos
-url: http://mf2.dit.ie:8080/geoserver/cso/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=cso:ltgeom&maxFeatures=50&outputFormat=json
-
-"""
-
 
 from collections import OrderedDict
 import fiona
@@ -247,9 +217,7 @@ def merge_polys(geojson, filter_key="", filter_value=""):
         quit()
 
 
-
 # def original_geojson(geojson):
-
 
 class MyGUI:
     """
@@ -259,12 +227,25 @@ class MyGUI:
 
     """
     def __init__(self, my_parent):
-        #Set up some instances variable interaction between the application and the window handler
 
-        self.single_plot = object()
-        self.first_shp_geojson = StringVar()
-        self.merge_from_shp = StringVar()
-        self.extract_points = object()
+        self.host_name = StringVar()
+        self.layer_id = StringVar()
+        self.srs_id = int()
+        self.property_id = StringVar()
+        self.geom_id = StringVar()
+        self.fproperty_id = StringVar()
+        self.fvalue_id1 = StringVar()
+        self.fvalue_id2 = StringVar()
+
+        PARAMS = {
+            "host": self.host_name,
+            "layer": self.layer_id,
+            "srs_code": self.srs_id,
+            "properties": [self.property_id],
+            "geom_field": self.geom_id,
+            "filter_property": self.fproperty_id,
+            "filter_values": [self.fvalue_id1, self.fvalue_id2]
+        }
 
         self.my_parent = my_parent
 
@@ -275,44 +256,82 @@ class MyGUI:
         self.frame1 = ttk.Frame(my_parent, padding=5, border=1)
         self.frame1.grid(row=0, column=0)
 
-        self.tasks_frame = LabelFrame(self.frame1, padx=15, pady=15, text="Tasks")
-        self.tasks_frame.grid(row=0, column=0, stick=NW)
+        self.tasks_frame = LabelFrame(self.frame1, padx=15, pady=15, text="Tasks 1")
+        self.tasks_frame.grid(row=0, column=0, sticky=NW)
 
-        Label(self.tasks_frame, text="1.").grid(row=0, column=0, sticky=W)
-        Button(self.tasks_frame, text="Please select Source Geojson", command=self.get_jsonFile)\
-            .grid(row=0, column=1, sticky=NW, pady=5)
+        Label(self.tasks_frame, text="1.", justify=LEFT).grid(row=0, column=0, sticky=W)
+        Label(self.tasks_frame, text="Host Name", justify=LEFT).grid(row=0, column=1, sticky=W)
+        Entry(self.tasks_frame, width=30, textvariable=self.host_name).grid(row=1, column=1, sticky=W)
 
-        Label(self.tasks_frame, text="2.").grid(row=1, column=0, sticky=W)
-        Button(self.tasks_frame, text="Plot Single Polygon", command=self.get_single_plot) \
-            .grid(row=1, column=1, sticky=NW, pady=5)
+        Label(self.tasks_frame, text="2.", justify=LEFT).grid(row=2, column=0, sticky=W)
+        Label(self.tasks_frame, text="Layer", justify=LEFT).grid(row=2, column=1, sticky=W)
+        Label(self.tasks_frame, text="Source Code", justify=LEFT).grid(row=2, column=2, sticky=W)
+        Entry(self.tasks_frame, width=20, textvariable=self.layer_id).grid(row=3, column=1, sticky=W)
+        Entry(self.tasks_frame, width=20, textvariable=self.srs_id).grid(row=3, column=2, sticky=W)
 
-        Label(self.tasks_frame, text="3.").grid(row=2, column=0, sticky=W)
-        Button(self.tasks_frame, text="Compute Centroids", command= self.centroid_from_merge) \
-            .grid(row=2, column=1, sticky=NW, pady=5)
+        Label(self.tasks_frame, text="3", justify=LEFT).grid(row=4, column=0, sticky=W)
+        Label(self.tasks_frame, text="Properties", justify=LEFT).grid(row=4, column=1, sticky=W)
+        Label(self.tasks_frame, text="Geometry field", justify=LEFT).grid(row=4, column=2, sticky=W)
+        Entry(self.tasks_frame, width=20, textvariable=self.property_id).grid(row=5, column=1, sticky=W)
+        Entry(self.tasks_frame, width=20, textvariable=self.geom_id).grid(row=5, column=2, sticky=W)
 
-        Label(self.tasks_frame, text="4.").grid(row=3, column=0, sticky=W)
-        Button(self.tasks_frame, text="Extract points", command= self.extract_points) \
-            .grid(row=3, column=1, sticky=NW, pady=5)
+        Label(self.tasks_frame, text="Task 1", justify=RIGHT).grid(row=6, column=1, sticky=W)
+        Button(self.tasks_frame, text="Get Geojson File") \
+            .grid(row=6, column=2, sticky=NW, pady=5)
 
-        Label(self.tasks_frame, text="5.").grid(row=4, column=0, sticky=W)
-        Button(self.tasks_frame, text="Convex Hull") \
-            .grid(row=4, column=1, sticky=NW, pady=5)
+        Label(self.tasks_frame, text="", justify=LEFT).grid(row=9, column=0, sticky=W)
+        Label(self.tasks_frame, text="4", justify=LEFT).grid(row=11, column=0, sticky=W)
+        Label(self.tasks_frame, text="Filter property Name", justify=LEFT).grid(row=11, column=1, sticky=W)
+        Entry(self.tasks_frame, width=20, textvariable=self.fproperty_id).grid(row=12, column=1, sticky=W)
 
-        Label(self.tasks_frame, text="6.").grid(row=5, column=0, sticky=W)
-        Button(self.tasks_frame, text="Distance Between Centroids") \
-            .grid(row=5, column=1, sticky=NW, pady=5)
+        Label(self.tasks_frame, text="5", justify=RIGHT).grid(row=13, column=0, sticky=W)
+        Label(self.tasks_frame, text="Filter Value 1", justify=LEFT).grid(row=13, column=1, sticky=W)
+        Label(self.tasks_frame, text="Filter Value 2", justify=LEFT).grid(row=13, column=2, sticky=W)
+        Entry(self.tasks_frame, width=20, textvariable=self.fvalue_id1).grid(row=14, column=1, sticky=W)
+        Entry(self.tasks_frame, width=20, textvariable=self.fvalue_id2).grid(row=14, column=2, sticky=W)
 
-        Label(self.tasks_frame, text="7.").grid(row=6, column=0, sticky=W)
-        Button(self.tasks_frame, text="Line Between Centroids") \
-            .grid(row=6, column=1, sticky=NW, pady=5)
+        Label(self.tasks_frame, text="TASK 2", justify=RIGHT).grid(row=15, column=1, sticky=W)
+        Button(self.tasks_frame, text="Plot Centroid") \
+            .grid(row=15, column=2, sticky=NW, pady=5)
 
-        Label(self.tasks_frame, text="8.").grid(row=7, column=0, sticky=W)
-        Button(self.tasks_frame, text="Geocode Centroids") \
-            .grid(row=7, column=1, sticky=NW, pady=5)
 
-        Label(self.tasks_frame, text="9.").grid(row=8, column=0, sticky=W)
-        Button(self.tasks_frame, text="Generate Shapefiles") \
-            .grid(row=8, column=1, sticky=NW, pady=5)
+        # Label(self.tasks_frame, text="1.").grid(row=2, column=0, sticky=W)
+        # Button(self.tasks_frame, text="Please select Source Geojson", command=self.get_jsonFile)\
+        #     .grid(row=2, column=1, sticky=NW, pady=5)
+        #
+        #
+        # Label(self.tasks_frame, text="2.").grid(row=3, column=0, sticky=W)
+        # Button(self.tasks_frame, text="Plot Single Polygon", command=self.get_single_plot) \
+        #     .grid(row=3, column=1, sticky=NW, pady=5)
+
+        # Label(self.tasks_frame, text="3.").grid(row=2, column=0, sticky=W)
+        # Button(self.tasks_frame, text="Compute Centroids", command= self.centroid_plot) \
+        #     .grid(row=2, column=1, sticky=NW, pady=5)
+        #
+        # Label(self.tasks_frame, text="4.").grid(row=3, column=0, sticky=W)
+        # Button(self.tasks_frame, text="Extract points", command= self.extract_points) \
+        #     .grid(row=3, column=1, sticky=NW, pady=5)
+        #
+        # Label(self.tasks_frame, text="5.").grid(row=4, column=0, sticky=W)
+        # Button(self.tasks_frame, text="Convex Hull") \
+        #     .grid(row=4, column=1, sticky=NW, pady=5)
+        #
+        # Label(self.tasks_frame, text="6.").grid(row=5, column=0, sticky=W)
+        # Button(self.tasks_frame, text="Distance Between Centroids") \
+        #     .grid(row=5, column=1, sticky=NW, pady=5)
+        #
+        # Label(self.tasks_frame, text="7.").grid(row=6, column=0, sticky=W)
+        # Button(self.tasks_frame, text="Line Between Centroids") \
+        #     .grid(row=6, column=1, sticky=NW, pady=5)
+        #
+        # Label(self.tasks_frame, text="8.").grid(row=7, column=0, sticky=W)
+        # Button(self.tasks_frame, text="Geocode Centroids") \
+        #     .grid(row=7, column=1, sticky=NW, pady=5)
+        #
+        # Label(self.tasks_frame, text="9.").grid(row=8, column=0, sticky=W)
+        # Button(self.tasks_frame, text="Generate Shapefiles") \
+        #     .grid(row=8, column=1, sticky=NW, pady=5)
+
 
         self.log_frame = LabelFrame(self.frame1, padx=15, pady=15, text="Log")
         self.log_frame.grid(row=0, column=1, sticky=NW)
@@ -323,6 +342,90 @@ class MyGUI:
     def catch_destroy(self):
         if messagebox.askokcancel("Quit", "Do you really wantto terminate the processs"):
             self.my_parent.destroy()
+
+    def get_geojson(params):
+
+        import urllib.parse
+        import httplib2
+        import os, os.path
+        import json
+        import xml.etree.ElementTree as etree
+
+        #
+        # Check that the parameters exist and/or sensible. Because the filter can contain some 'odd' characters such as '%'
+        # and single quotes the filter text needs to be url encoded so that text like "countyname LIKE '%Cork%'" becomes
+        # "countyname%20LIKE%20%27%25Cork%25%27" which is safer for URLs
+        #
+        if "host" not in params:
+            raise ValueError("Value for 'host' required")
+        if "layer" not in params:
+            raise ValueError("Value for 'layer' required")
+        if "srs_code" in params and params["srs_code"]:
+            srs_text = "&srsName=epsg:{}".format(params["srs_code"])
+        else:
+            srs_text = ""
+        if "properties" in params and params["properties"]:
+            item_string = ""
+            for item in params["properties"]:
+                item_string += str(item) + ","
+            if "geom_field" in params and params["geom_field"]:
+                item_string += str(params["geom_field"])
+            property_text = "&PROPERTYNAME={}".format(item_string)
+        else:
+            property_text = ""
+        if "filter_property" in params and params["filter_property"] and params["filter_values"]:
+            filter_text = "{filter_property} LIKE '%{filter_values}%'".format(filter_property=params["filter_property"],
+                                                                              filter_values=params["filter_values"][0])
+            for item in range(1, len(params["filter_values"])):
+                filter_text += "OR {filter_property} LIKE '%{filter_values}%'".format(
+                    filter_property=params["filter_property"], filter_values=params["filter_values"][item])
+            filter_text = urllib.parse.quote(filter_text)
+            filter_text = "&CQL_FILTER=" + filter_text
+        else:
+            filter_text = ""
+
+        url = "http://{host}/geoserver/ows?" \
+              "service=WFS&version=1.0.0&" \
+              "request=GetFeature&" \
+              "typeName={layer}&" \
+              "outputFormat=json".format(host=params["host"], layer=params["layer"])
+        url += srs_text
+        url += property_text
+        url += filter_text
+
+        #
+        # Make a directory to hold downloads so that we don't have to repeatedly download them later, i.e. they already
+        # exist so we get them from a local directory. This directory is called .httpcache".
+        #
+        scriptDir = os.path.dirname(__file__)
+        cacheDir = os.path.normpath(os.path.join(scriptDir, ".httpcache"))
+        if not os.path.exists(cacheDir):
+            os.mkdir(cacheDir)
+
+        #
+        # Go to the web and attempt to get the resource
+        #
+        try:
+            h = httplib2.Http()
+            response_headers, response = h.request(url)
+            response = response.decode()
+
+            #
+            # Geoserver only sends valid data in the requested format, in our case GeoJSON, so if we get a response back in
+            # XML format we know that we have an error. We do minimal parsing on the xml to extract the error text and raise
+            # an exception based on it.
+            #
+            if response[:5] == "<?xml":
+                response = etree.fromstring(response)
+                xml_error = ""
+                for element in response:
+                    xml_error += element.text
+                raise Exception(xml_error)
+            else:
+                return json.loads(response)
+
+        except httplib2.HttpLib2Error as e:
+            print(e)
 
     def get_jsonFile(self):
         # Can run file dialogue from anywhere
@@ -352,9 +455,12 @@ class MyGUI:
     def get_single_plot(self):
         try:
             source = gp.read_file(Source_file)
-            # print(source.head())
             source.plot(figsize=(10, 10), alpha=.7, edgecolor='k')
             plt.show()
+
+            self.log_text.insert(
+                END, " Single polygon Successful ploted\n"
+            )
 
         except Exception as e:
             print(e)
@@ -382,37 +488,31 @@ class MyGUI:
             self.log_text.insert(END, "-" * 80 + "\n")
             return
 
-    def centroid_from_merge(self):
+    def centroid_plot(self):
+        source = gp.read_file(Source_file)
+        ax = source.plot(figsize=(10, 10), alpha=.7, edgecolor='k')
+        source_centroid = source.centroid
+        source_centroid.plot(ax=ax, color='red')
+        print(" Begining of centroid Values------------------------------------ \n")
+        print(source_centroid)
+        print("End of Centroid Values ------------------------------- \n")
         try:
-            # if not self.merged:
-            #     raise ValueError("Couldn't find merged GeoJSON.")
-            #
-            # self.log_text.insert(END, "-" * 80 + "\n")
+            source = gp.read_file(Source_file)
+            ax = source.plot(figsize=(10, 10), alpha=.7, edgecolor='k')
+            source_centroid = source.centroid
+            source_centroid.plot(ax=ax, color='red')
 
-            # Active ingredient...
-            self.merged = merge_polys(Source_file)
-            self.centroid = make_centroid(self.merged)
+            print(" Begining of centroid Values------------------------------------ \n")
+            print(source_centroid)
+            print("End of Centroid Values ------------------------------- \n")
 
             self.log_text.insert(
-                END, "Centroid: {}\n{}\n"
-                    .format(
-                    self.centroid["features"][0]["geometry"]["coordinates"],
-                    self.centroid["features"][0]["properties"]["address"]
-                )
+                END, " Centroid computed successfully"
             )
 
-            # Active ingredient...
-            geojson_to_shp(self.centroid, "{}/{}.shp".format(OUTPUT_DIR, self.centoid_shp_name.get()))
-
-            self.log_text.insert(END, "Finished creating new shapefile: {}/{}.shp\n".format(OUTPUT_DIR,
-                                                                                            self.centoid_shp_name.get()))
-            self.log_text.insert(END, "-" * 80 + "\n")
-            return
         except Exception as e:
-            self.log_text.insert(END, "-" * 80 + "\n")
-            self.log_text.insert(END, "Ops there is an Error: {}\n".format(e))
-            self.log_text.insert(END, "-" * 80 + "\n")
-            return
+            print(e)
+            quit()
 
 
     def extract_points(self):
